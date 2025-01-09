e2941c990e1e011303e98d5eabdbf08d5cf28f0e
unknown <emmco96@gmail.com>
2025-01-09 21:51:05 +0100

    new

--- script.js
+++ script.js
@@ -45,25 +45,25 @@ document.addEventListener('DOMContentLoaded', function () {
   
   
   
-  document.addEventListener('DOMContentLoaded', function() {
-      const sections = document.querySelectorAll('div'); // Selecting by ID
+  // document.addEventListener('DOMContentLoaded', function() {
+  //     const sections = document.querySelectorAll('diva'); // Selecting by ID
     
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-          } else {
-            entry.target.classList.remove('visible');
-          }
-        });
-      }, {
-        threshold: 0.1
-      });
+  //     const observer = new IntersectionObserver((entries) => {
+  //       entries.forEach(entry => {
+  //         if (entry.isIntersecting) {
+  //           entry.target.classList.add('visible');
+  //         } else {
+  //           entry.target.classList.remove('visible');
+  //         }
+  //       });
+  //     }, {
+  //       threshold: 0.1
+  //     });
     
-      sections.forEach(section => {
-        observer.observe(section);
-      });
-    });
+  //     sections.forEach(section => {
+  //       observer.observe(section);
+  //     });
+  //   });
     
   
 document.addEventListener("DOMContentLoaded", function() {
